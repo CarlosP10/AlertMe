716c25446f0b6c5fde4bfc12ec64ec7996e9cb76
CarlosP10 <00028516@uca.edu.sv>
2020-11-26 13:21:26 -0600

    map

--- src/pages/AddReport/components/addMapModule.js
+++ src/pages/AddReport/components/addMapModule.js
@@ -1,41 +1,65 @@
-import { Alert, View } from 'react-native'
+import {Alert, Button, Text, View} from 'react-native';
+import React, {PROVIDER_GOOGLE, useState} from 'react';
 
+import Geolocation from '@react-native-community/geolocation';
+import MapView from 'react-native-maps';
 import Option from './option';
-import {PrimaryText} from '@common'
-import React, { PROVIDER_GOOGLE } from 'react'
+import {PrimaryText} from '@common';
 import styles from '../styles/addMap';
 
-import MapView from 'react-native-maps'
-
 const addMapModule = () => {
+  const [error, setError] = useState('');
+  const [position, setPosition] = useState({
+    latitude: 0,
+    longitude: 0,
+  });
 
-    
+  const getPosition = () => {
+    Geolocation.getCurrentPosition(
+      (pos) => {
+        setError('');
+        setPosition({
+          latitude: pos.coords.latitude,
+          longitude: pos.coords.longitude,
+        });
+      },
+      (e) => {
+        setError(e.message), console.log(e);
+      },
+    );
+  };
 
-    return (
-        <View style={styles.mapContainer}>
-            <Option
-            iconName="location-outline"
-            title=" Agregar Localización"
-            typeIcon="ionicon">
-          </Option>
-          
-          <View style={styles.mapView}>
-          <MapView
+  return (
+    <View style={styles.mapContainer}>
+      <Option
+        iconName="location-outline"
+        title=" Agregar Localización"
+        typeIcon="ionicon"></Option>
+
+      <View>
+        <Button title="Get Current Position" onPress={getPosition} />
+        {error ? (
+          <Text>Error retrieving current position</Text>
+        ) : (
+          <>
+            <Text>Latitude: {position.latitude}</Text>
+            <Text>Longitude: {position.longitude}</Text>
+          </>
+        )}
+      </View>
+      <View style={styles.mapView}>
+        <MapView
           provider={PROVIDER_GOOGLE}
           style={styles.map}
           region={{
-              latitude: 37.78825,
-              longitude: -122.4324,
-              latitudeDelta: 0.09,
-              longitudeDelta: 0.035
-          }}>
+            latitude: position.latitude,
+            longitude: position.longitude,
+            //   latitudeDelta: 0.09,
+            //   longitudeDelta: 0.035
+          }}></MapView>
+      </View>
+    </View>
+  );
+};
 
-          </MapView>
-          </View>
-
-        </View>
-    )
-}
-
-
-export default addMapModule
+export default addMapModule;
--- src/pages/AddReport/index.js
+++ src/pages/AddReport/index.js
@@ -3,9 +3,7 @@ import {ScrollView, Text, View} from 'react-native';
 
 import AddMapModule from './components/addMapModule';
 import AddPicModule from './components/addPicModule';
-import CurrentPosition from './components/currentPos';
 import Footer from '@components/footer';
-import Geolocation from '@react-native-community/geolocation';
 import {Input} from 'react-native-elements';
 import Option from './components/option';
 import styles from './styles/index';
@@ -23,9 +21,6 @@ const index = () => {
               typeIcon="ionicon">
               <Input placeholder="Ingrese el suceso" />
             </Option>
-          </View>
-          <View>
-            <CurrentPosition />
           </View>
           <AddMapModule />
           <AddPicModule />
